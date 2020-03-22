8ad0b35cf181df8d0ddc05e5e03dcd625618c048
Matthew Burke <matthewpatrickburke@gmail.com>
2020-03-22 15:23:20 -0600

    Solve day 22 part 1

--- src/com/mwpb/Aoc22.java
+++ src/com/mwpb/Aoc22.java
@@ -47,7 +47,126 @@ public class Aoc22 {
 	}
 	
 	void dealWithIncrement(int n) {
-		// implement
+		Integer[] table = new Integer[this.deck.size()];
+		for (int i = 0; i < this.numberOfCards; i++) {
+			int card = this.deck.pop();
+			table[(n * i) % this.numberOfCards] = card;
+		}
+		List<Integer> list = Arrays.asList(table);
+		this.deck = new ArrayDeque<Integer>(list);
+	}
+	
+	void carryOutInstructions() {
+		this.dealWithIncrement(15);
+		this.cut(-4394);
+		this.dealWithIncrement(9);
+		this.dealIntoNewStack();
+		this.cut(-8068);
+		this.dealWithIncrement(15);
+		this.dealIntoNewStack();
+		this.cut(1470);
+		this.dealIntoNewStack();
+		this.cut(4151);
+		this.dealIntoNewStack();
+		this.cut(-2438);
+		this.dealIntoNewStack();
+		this.cut(9852);
+		this.dealWithIncrement(50);
+		this.cut(-953);
+		this.dealWithIncrement(8);
+		this.cut(-2836);
+		this.dealWithIncrement(30);
+		this.cut(-2419);
+		this.dealIntoNewStack();
+		this.cut(2759);
+		this.dealWithIncrement(66);
+		this.cut(1127);
+		this.dealWithIncrement(66);
+		this.cut(2194);
+		this.dealWithIncrement(48);
+		this.cut(4710);
+		this.dealWithIncrement(49);
+		this.dealIntoNewStack();
+		this.dealWithIncrement(59);
+		this.dealIntoNewStack();
+		this.dealWithIncrement(25);
+		this.dealIntoNewStack();
+		this.dealWithIncrement(60);
+		this.cut(-2003);
+		this.dealWithIncrement(2);
+		this.cut(-6166);
+		this.dealWithIncrement(26);
+		this.cut(-6179);
+		this.dealWithIncrement(4);
+		this.cut(373);
+		this.dealWithIncrement(53);
+		this.cut(6849);
+		this.dealWithIncrement(13);
+		this.cut(625);
+		this.dealWithIncrement(68);
+		this.cut(4084);
+		this.dealWithIncrement(53);
+		this.cut(-6939);
+		this.dealIntoNewStack();
+		this.cut(-3416);
+		this.dealWithIncrement(39);
+		this.cut(-2671);
+		this.dealWithIncrement(64);
+		this.dealIntoNewStack();
+		this.dealWithIncrement(75);
+		this.cut(7654);
+		this.dealIntoNewStack();
+		this.cut(-5431);
+		this.dealWithIncrement(66);
+		this.cut(-370);
+		this.dealIntoNewStack();
+		this.cut(3316);
+		this.dealWithIncrement(31);
+		this.cut(312);
+		this.dealWithIncrement(22);
+		this.cut(71);
+		this.dealWithIncrement(21);
+		this.cut(562);
+		this.dealWithIncrement(27);
+		this.cut(8611);
+		this.dealWithIncrement(67);
+		this.cut(8358);
+		this.dealWithIncrement(7);
+		this.cut(-2957);
+		this.dealWithIncrement(71);
+		this.cut(1740);
+		this.dealWithIncrement(31);
+		this.cut(-9577);
+		this.dealWithIncrement(59);
+		this.cut(6104);
+		this.dealWithIncrement(40);
+		this.cut(-8862);
+		this.dealWithIncrement(17);
+		this.cut(2516);
+		this.dealWithIncrement(34);
+		this.cut(9594);
+		this.dealIntoNewStack();
+		this.cut(5182);
+		this.dealWithIncrement(72);
+		this.cut(-2630);
+		this.dealIntoNewStack();
+		this.cut(-9018);
+		this.dealWithIncrement(45);
+		this.cut(-1069);
+		this.dealWithIncrement(28);
+		this.cut(358);
+		this.dealIntoNewStack();
+		this.cut(-2244);
+	}
+	
+	int get2019th() {
+		for (int i = 0; i< this.numberOfCards; i++) {
+			int card = this.deck.pop();
+			if (card == 2019) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 }
--- test/com/mwpb/Aoc22Test.java
+++ test/com/mwpb/Aoc22Test.java
@@ -1,5 +1,6 @@
 package com.mwpb;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Aoc22Test {
@@ -33,4 +34,106 @@ public class Aoc22Test {
 		System.out.println(aoc22_2.deck);
 	}
 	
+	@Test
+	void testDealWithIncrement() {
+		Aoc22 aoc22 = new Aoc22(10);
+		System.out.println(aoc22.deck);
+		aoc22.dealWithIncrement(3);
+		System.out.println(aoc22.deck);
+	}
+	
+	@Test
+	void firstTest() {
+		Aoc22 aoc22 = new Aoc22(10);
+		System.out.println(aoc22.deck);
+		aoc22.dealWithIncrement(7);
+		aoc22.dealIntoNewStack();
+		aoc22.dealIntoNewStack();
+		System.out.println(aoc22.deck);
+	}
+	
+	@Test
+	void secondTest() {
+		Aoc22 aoc22 = new Aoc22(10);
+		System.out.println(aoc22.deck);
+		aoc22.cut(6);
+		aoc22.dealWithIncrement(7);
+		aoc22.dealIntoNewStack();
+		System.out.println(aoc22.deck);
+	}
+	
+	@Test
+	void thirdTest() {
+		Aoc22 aoc22 = new Aoc22(10);
+		System.out.println(aoc22.deck);
+		aoc22.dealWithIncrement(7);
+		aoc22.dealWithIncrement(9);
+		aoc22.cut(-2);
+		System.out.println(aoc22.deck);
+	}
+	
+	@Test
+	void fourthTest() {
+		Aoc22 aoc22 = new Aoc22(10);
+		System.out.println(aoc22.deck);
+		aoc22.dealIntoNewStack();
+		aoc22.cut(-2);
+		aoc22.dealWithIncrement(7);
+		aoc22.cut(8);
+		aoc22.cut(-4);
+		aoc22.dealWithIncrement(7);
+		aoc22.cut(3);
+		aoc22.dealWithIncrement(9);
+		aoc22.dealWithIncrement(3);
+		aoc22.cut(-1);
+		System.out.println(aoc22.deck);
+	}
+	
+	@Test
+	void testFullInstructions() {
+		Aoc22 aoc22 = new Aoc22(10007);
+		System.out.println(aoc22.deck);
+		aoc22.carryOutInstructions();
+		System.out.println(aoc22.deck);
+		Assertions.assertEquals(4485, aoc22.get2019th());
+	}
+	
+	@Test
+	void testHugeShuffle() {
+		Aoc22 aoc22 = new Aoc22(10007);
+		System.out.println(aoc22.deck);
+		for (Long i = 0L; i < 101741582076661L; i++) {
+			aoc22.carryOutInstructions();
+		}
+		System.out.println(aoc22.deck);
+		Assertions.assertEquals(4485, aoc22.get2019th());
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
