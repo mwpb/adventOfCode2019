94f22b1917764474340d968e4318d600884c0269
Matthew Burke <matthewpatrickburke@gmail.com>
2020-04-04 10:19:40 -0600

    Solve day 23

---
+++ test/com/mwpb/Aoc23Test.java
@@ -0,0 +1,16 @@
+package com.mwpb;
+
+import org.junit.jupiter.api.Test;
+
+public class Aoc23Test {
+	@Test
+	void testBoot() {
+		Aoc23 aoc23 = new Aoc23();
+	}
+
+	@Test
+	void testGo() {
+		Aoc23 aoc23 = new Aoc23();
+		aoc23.go();
+	}
+}
